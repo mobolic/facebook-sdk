bef40be04b7230a76c23608f406887d3cf1c310c
Ben Firshman <ben@firshman.co.uk>
2010-04-22 19:35:45 +0800

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+ 
+setup(
+    name='facebook-python-sdk',
+    version='0.1',
+    description='This client library is designed to support the Facebook Graph API and the official Facebook JavaScript SDK, which is the canonical way to implement Facebook authentication.',
+    author='Facebook',
+    url='http://github.com/facebook/python-sdk',
+    package_dir={'': 'src'},
+    py_modules=[
+        'facebook',
+    ],
+)
