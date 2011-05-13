309ef1dca602e0ef027c7c5a8d1a28d91fcbe898
Aidan Lister <aidan@php.net>
2011-05-13 11:21:49 +0200

    Move facebook.py out of "src" directory as per http://as.ynchrono.us/2007/12/filesystem-structure-of-python-project_21.html

--- setup.py
+++ setup.py
@@ -4,11 +4,10 @@ from distutils.core import setup
  
 setup(
     name='facebook-python-sdk',
-    version='0.1.2',
+    version='0.2.0',
     description='This client library is designed to support the Facebook Graph API and the official Facebook JavaScript SDK, which is the canonical way to implement Facebook authentication.',
     author='Facebook',
-    url='http://github.com/facebook/python-sdk',
-    package_dir={'': 'src'},
+    url='http://github.com/pythonforfacebook/facebook-sdk',
     py_modules=[
         'facebook',
     ],
