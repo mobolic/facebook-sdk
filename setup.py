b5ee678115391885a00cfd1ee114c6b5b22f7a55
Aidan Lister <aidan@php.net>
2011-05-17 11:21:40 +0200

    Rename the package so we can push to PyPi

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 from distutils.core import setup
  
 setup(
-    name='facebook-python-sdk',
+    name='facebook-sdk',
     version='0.2.0',
     description='This client library is designed to support the Facebook Graph API and the official Facebook JavaScript SDK, which is the canonical way to implement Facebook authentication.',
     author='Facebook',
@@ -12,3 +11,4 @@ setup(
         'facebook',
     ],
 )
+
