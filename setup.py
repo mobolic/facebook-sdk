faaf64ef3c1ca9aaefe78789655c092de94cf38d
Kit Sunde <kitsunde@mediapopinc.com>
2011-10-03 05:01:40 +0800

    Updated author, version and set the root directory to src/

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from distutils.core import setup
  
 setup(
     name='facebook-sdk',
-    version='0.2.0',
+    version='0.3.0',
     description='This client library is designed to support the Facebook Graph API and the official Facebook JavaScript SDK, which is the canonical way to implement Facebook authentication.',
-    author='Facebook',
-    url='http://github.com/pythonforfacebook/facebook-sdk',
+    author='Kit Sunde',
+    author_email='kitsunde@gmail.com',
+    url='http://github.com/Celc/facebook-sdk',
+    package_dir={'': 'src'},
     py_modules=[
         'facebook',
     ],
 )
-
