15d284f3eac1941d763b7c4ad6491c65d870fb75
Martey Dodoo <martey@marteydodoo.com>
2012-04-25 05:34:03 -0400

    Changes to upload to PyPi.
    Add slightly more complete setup.py, a MANIFEST.in to add the examples
    directory, and a new version number to celebrate all of the work that
    has been done in the last 11 months.

--- setup.py
+++ setup.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python
 from distutils.core import setup
+import os
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 
 setup(
     name='facebook-sdk',
-    version='0.2.0',
+    version='0.3.0',
     description='This client library is designed to support the Facebook '
                 'Graph API and the official Facebook JavaScript SDK, which '
                 'is the canonical way to implement Facebook authentication.',
     author='Facebook',
     url='http://github.com/pythonforfacebook/facebook-sdk',
+    license='Apache',
     py_modules=[
         'facebook',
+    ],
+    long_description=read("readme.md"),
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
     ],
 )
