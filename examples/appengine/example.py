392e993eb7809cc26baaf6348d7a5f6c0ede427f
Jose Asuncion <jose.asuncion@gmail.com>
2012-11-24 16:33:36 +0800

    use webapp2 sessions to cache user

--- examples/appengine/example.py
+++ examples/appengine/example.py
@@ -55,29 +55,52 @@ class BaseHandler(webapp.RequestHandler):
     """
     @property
     def current_user(self):
-        if not hasattr(self, "_current_user"):
-            self._current_user = None
-            cookie = facebook.get_user_from_cookie(
-                self.request.cookies, FACEBOOK_APP_ID, FACEBOOK_APP_SECRET)
+        if self.session.get("user"):
+            # User is logged in
+            return self.session.get("user")
+        else:
+            # Either used just logged in or just saw the first page
+            # We'll see here 
+            cookie = facebook.get_user_from_cookie(self.request.cookies, FACEBOOK_APP_ID, FACEBOOK_APP_SECRET)
+            
             if cookie:
-                # Store a local instance of the user data so we don't need
-                # a round-trip to Facebook on every request
+                # Okay so user logged in 
+                # Now, check to see if existing user
                 user = User.get_by_key_name(cookie["uid"])
                 if not user:
+                    # Not an existing user so get user info
                     graph = facebook.GraphAPI(cookie["access_token"])
                     profile = graph.get_object("me")
                     user = User(key_name=str(profile["id"]),
-                                id=str(profile["id"]),
-                                name=profile["name"],
-                                profile_url=profile["link"],
-                                access_token=cookie["access_token"])
+                        id=str(profile["id"]),
+                        name=profile["name"],
+                        profile_url=profile["link"],
+                        access_token=cookie["access_token"])
                     user.put()
                 elif user.access_token != cookie["access_token"]:
                     user.access_token = cookie["access_token"]
                     user.put()
-                self._current_user = user
-        return self._current_user
+                # User is now logged in
+                self.session["user"] = user.to_session()
+                return self.session.get("user")
+        return None
+ 
+    def dispatch(self):
+        """ This snippet of code is taken from the webapp2 framework documentation.
+        See more at http://webapp-improved.appspot.com/api/webapp2_extras/sessions.html
+        """
+        self.session_store = sessions.get_store(request=self.request)   
+        try:
+            webapp2.RequestHandler.dispatch(self)
+        finally:
+            self.session_store.save_sessions(self.response)
 
+    @webapp2.cached_property
+    def session(self):
+        """ This snippet of code is taken from the webapp2 framework documentation.
+        See more at http://webapp-improved.appspot.com/api/webapp2_extras/sessions.html
+        """
+        return self.session_store.get_session()
 
 class HomeHandler(BaseHandler):
     def get(self):
