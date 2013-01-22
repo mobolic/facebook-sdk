6a1adf963266da6ecd609c4c04d1a8d5edae8657
Bojan Radojevic <bradojevic@gmail.com>
2013-01-22 16:26:13 +0100

    Added support for video upload.

--- facebook.py
+++ facebook.py
@@ -233,6 +233,48 @@ class GraphAPI(object):
 
         return response
 
+    def put_video(self, video, message=None, album_id=None, **kwargs):
+        """Uploads an video using multipart/form-data.
+
+        video=File like object for the video
+        message=Caption for your video
+        album_id=None posts to /me/videos which uses or creates and uses
+        an album for your application.
+
+        """
+        object_id = album_id or "me"
+        #it would have been nice to reuse self.request;
+        #but multipart is messy in urllib
+        post_args = {
+            'access_token': self.access_token,
+            'source': video,
+            'message': message,
+        }
+        post_args.update(kwargs)
+        content_type, body = self._encode_multipart_form(post_args)
+        req = urllib2.Request(("https://graph.facebook.com/%s/videos" %
+                               object_id),
+                              data=body)
+        req.add_header('Content-Type', content_type)
+        try:
+            data = urllib2.urlopen(req).read()
+        #For Python 3 use this:
+        #except urllib2.HTTPError as e:
+        except urllib2.HTTPError, e:
+            data = e.read()  # Facebook sends OAuth errors as 400, and urllib2
+                             # throws an exception, we want a GraphAPIError
+        try:
+            response = _parse_json(data)
+            # Raise an error if we got one, but don't not if Facebook just
+            # gave us a Bool value
+            if (response and isinstance(response, dict) and
+                    response.get("error")):
+                raise GraphAPIError(response)
+        except ValueError:
+            response = data
+
+        return response
+
     # based on: http://code.activestate.com/recipes/146306/
     def _encode_multipart_form(self, fields):
         """Encode files as 'multipart/form-data'.
