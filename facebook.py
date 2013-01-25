d317e146b7834aa4ecc6ab167a9cd3f5f0fde6c5
Bojan Radojevic <bradojevic@gmail.com>
2013-01-25 10:58:41 +0100

    Removed invalid fields.

--- facebook.py
+++ facebook.py
@@ -233,22 +233,19 @@ class GraphAPI(object):
 
         return response
 
-    def put_video(self, video, message=None, album_id=None, **kwargs):
+    def put_video(self, video, message=None, **kwargs):
         """Uploads an video using multipart/form-data.
-
         video=File like object for the video
         message=Caption for your video
-        album_id=None posts to /me/videos which uses or creates and uses
-        an album for your application.
 
         """
-        object_id = album_id or "me"
+        object_id = "me"
         #it would have been nice to reuse self.request;
         #but multipart is messy in urllib
         post_args = {
             'access_token': self.access_token,
             'source': video,
-            'message': message,
+            'description': message,
         }
         post_args.update(kwargs)
         content_type, body = self._encode_multipart_form(post_args)
@@ -261,7 +258,7 @@ class GraphAPI(object):
         #For Python 3 use this:
         #except urllib2.HTTPError as e:
         except urllib2.HTTPError, e:
-            data = e.read()  # Facebook sends OAuth errors as 400, and urllib2
+            data = e.read() # Facebook sends OAuth errors as 400, and urllib2
                              # throws an exception, we want a GraphAPIError
         try:
             response = _parse_json(data)
