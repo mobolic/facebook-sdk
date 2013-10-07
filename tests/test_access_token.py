ee1ade300ec99223d11e0c3531712b3c4909df50
Nick Pack <nick@nickpack.com>
2013-10-08 00:40:28 +0100

    Reorganised tests

--- tests/test_access_token.py
+++ tests/test_access_token.py
@@ -13,19 +13,23 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-import os
+import facebook
 import unittest
+from tests.test_facebook import FacebookTestCase
 
 
-class FacebookTestCase(unittest.TestCase):
+class TestGetAppAccessToken(FacebookTestCase):
     """
-    Sets up application ID and secret from environment.
-    """
-    def setUp(self):
-        try:
-            self.app_id = os.environ["FACEBOOK_APP_ID"]
-            self.secret = os.environ["FACEBOOK_SECRET"]
-        except KeyError:
-            raise Exception("FACEBOOK_APP_ID and FACEBOOK_SECRET "
-                            "must be set as environmental variables.")
+    Test if application access token is returned properly.
 
+    Note that this only tests if the returned token is a string, not
+    whether it is valid.
+
+    """
+    def test_get_app_access_token(self):
+        assert(isinstance(facebook.get_app_access_token(
+            self.app_id, self.secret), str))
+
+
+if __name__ == '__main__':
+    unittest.main()
