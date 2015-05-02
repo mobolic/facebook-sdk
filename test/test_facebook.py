93a93050bd3841a357e534ce7c11425792a8e866
Martey Dodoo <martey@marteydodoo.com>
2015-05-02 02:30:08 -0400

    Remove version 1.0 test.

--- test/test_facebook.py
+++ test/test_facebook.py
@@ -58,10 +58,6 @@ class TestAPIVersion(FacebookTestCase):
         self.assertNotEqual(
             graph.version, "", "Version should not be an empty string.")
 
-    def test_version_1_0(self):
-        graph = facebook.GraphAPI(version=1.0)
-        self.assertEqual(graph.get_version(), 1.0)
-
     def test_version_2_0(self):
         graph = facebook.GraphAPI(version=2.0)
         self.assertEqual(graph.get_version(), 2.0)
