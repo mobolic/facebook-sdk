e2afe1f2ddc387e893bc2e166ff8a6d48dcac4bc
Martey Dodoo <martey@mobolic.com>
2015-10-11 14:05:26 -0400

    Add additional Python classifiers in setup.py.
    Fix #244. Add classifiers for generic Python 2 and 3 support, as well as
    a specific classifier for 3.4.

--- setup.py
+++ setup.py
@@ -18,9 +18,12 @@ setup(
     long_description=open("README.rst").read(),
     classifiers=[
         'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
     ],
     install_requires=[
         'requests',
