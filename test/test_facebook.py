3aa544faa24de83844e65d74e99fe2839d6f93ee
Pääkkönen Juho <paakkoj6@t31300-la048.org.aalto.fi>
2016-12-14 13:43:26 +0200

    Add tests for get_all_connections method.
    The tests create facebook test users and friend connections for those
    users. The get_all_connections method is tested to return a generator
    containing the right number of connections which include the right key
    strings.

--- test/test_facebook.py
+++ test/test_facebook.py
@@ -16,6 +16,7 @@
 import facebook
 import os
 import unittest
+import inspect
 
 try:
     from urllib.parse import parse_qs, urlencode, urlparse
@@ -231,6 +232,40 @@ class TestParseSignedRequest(FacebookTestCase):
         self.assertTrue('code' in result)
         self.assertTrue('user_id' in result)
         self.assertTrue('algorithm' in result)
+
+
+class TestGetAllConnectionsMethod(FacebookTestCase):
+
+    def test_function_with_zero_connections(self):
+        token = facebook.GraphAPI().get_app_access_token(
+            self.app_id, self.secret)
+        graph = facebook.GraphAPI(token)
+
+        self.create_test_users(self.app_id, graph, 1)
+        friends = graph.get_all_connections(self.test_users[0]['id'],
+                                            'friends')
+
+        self.assertTrue(inspect.isgenerator(friends))
+        self.assertTrue(len(list(friends)) == 0)
+
+    def test_function_returns_correct_connections(self):
+        token = facebook.GraphAPI().get_app_access_token(
+            self.app_id, self.secret)
+        graph = facebook.GraphAPI(token)
+
+        self.create_test_users(self.app_id, graph, 27)
+        self.create_friend_connections(self.test_users[0], self.test_users)
+
+        friends = graph.get_all_connections(self.test_users[0]['id'],
+                                            'friends')
+        self.assertTrue(inspect.isgenerator(friends))
+
+        friends_list = list(friends)
+        self.assertTrue(len(friends_list) == 26)
+        for f in friends:
+            self.assertTrue(isinstance(f, dict))
+            self.assertTrue('name' in f)
+            self.assertTrue('id' in f)
 
 
 if __name__ == '__main__':
