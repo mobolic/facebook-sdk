fd15a3abcb0e45d18a45bd7ec62f9bb17e071f62
Martey Dodoo <martey@mobolic.com>
2017-05-15 22:36:55 -0400

    Update changelog and add automated tests.

--- test/test_facebook.py
+++ test/test_facebook.py
@@ -234,6 +234,31 @@ class TestParseSignedRequest(FacebookTestCase):
         self.assertTrue('algorithm' in result)
 
 
+class TestSearchMethod(FacebookTestCase):
+    def setUp(self):
+        """Create GraphAPI object that can search (i.e. has user token)."""
+        super(TestSearchMethod, self).setUp()
+        # Create an app access token to create a test user
+        # to create a GraphAPI object with a valid user access token.
+        app_token = facebook.GraphAPI().get_app_access_token(self.app_id,
+                                                             self.secret,
+                                                             True)
+        self.create_test_users(self.app_id, facebook.GraphAPI(app_token), 1)
+        user = self.test_users[0]
+        self.graph = facebook.GraphAPI(user["access_token"])
+
+    def test_valid_search_types(self):
+        """Verify that search method accepts all valid search types."""
+        for search_type in facebook.VALID_SEARCH_TYPES:
+            self.graph.search(type=search_type, q="foobar")
+
+    def test_invalid_search_type(self):
+        """Verify that search method fails when an invalid type is passed."""
+        search_args = {"type": "foo", "q": "bar"}
+        self.assertRaises(
+            facebook.GraphAPIError, self.graph.search, search_args)
+
+
 class TestGetAllConnectionsMethod(FacebookTestCase):
 
     def test_function_with_zero_connections(self):
