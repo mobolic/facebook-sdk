d91e706245dc4471a56b19f5e207700490159cdc
Martey Dodoo <martey@mobolic.com>
2017-11-06 23:14:48 -0500

    Add support for Python 3.6.

--- setup.py
+++ setup.py
@@ -23,7 +23,8 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5'
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
     ],
     install_requires=[
         'requests',
