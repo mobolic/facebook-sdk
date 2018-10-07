ceb2253582418d1ff298081b28e2c25023990276
Martey Dodoo <martey@mobolic.com>
2018-10-06 22:07:28 -0400

    Reformat code with "black -l 79".

--- examples/flask/config.py
+++ examples/flask/config.py
@@ -3,8 +3,9 @@ from os import path
 # App details
 BASE_DIRECTORY = path.abspath(path.dirname(__file__))
 DEBUG = True
-SECRET_KEY = 'keep_it_like_a_secret'
+SECRET_KEY = "keep_it_like_a_secret"
 
 # Database details
-SQLALCHEMY_DATABASE_URI = '{0}{1}'.format('sqlite:///',
-                                          path.join(BASE_DIRECTORY, 'app.db'))
+SQLALCHEMY_DATABASE_URI = "{0}{1}".format(
+    "sqlite:///", path.join(BASE_DIRECTORY, "app.db")
+)
--- examples/get_posts.py
+++ examples/get_posts.py
@@ -12,18 +12,18 @@ def some_action(post):
     post's message (post['message']) or the post's picture (post['picture']).
     In this implementation we just print the post's created time.
     """
-    print(post['created_time'])
+    print(post["created_time"])
 
 
 # You'll need an access token here to do anything.  You can get a temporary one
 # here: https://developers.facebook.com/tools/explorer/
-access_token = ''
+access_token = ""
 # Look at Bill Gates's profile for this example by using his Facebook id.
-user = 'BillGates'
+user = "BillGates"
 
 graph = facebook.GraphAPI(access_token)
 profile = graph.get_object(user)
-posts = graph.get_connections(profile['id'], 'posts')
+posts = graph.get_connections(profile["id"], "posts")
 
 # Wrap this block in a while loop so we can keep paginating requests until
 # finished.
@@ -31,9 +31,9 @@ while True:
     try:
         # Perform some action on each post in the collection we receive from
         # Facebook.
-        [some_action(post=post) for post in posts['data']]
+        [some_action(post=post) for post in posts["data"]]
         # Attempt to make a request to the next page of data, if it exists.
-        posts = requests.get(posts['paging']['next']).json()
+        posts = requests.get(posts["paging"]["next"]).json()
     except KeyError:
         # When there are no more pages (['paging']['next']), break from the
         # loop and end the script.
