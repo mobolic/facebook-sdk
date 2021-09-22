77bbf5f2c68c740d2def0986c8c610cdc8c12b61
Robi Ng <robi@nugit.co>
2021-09-22 15:08:15 +0800

    Add support for Graph API v11.0

--- facebook/__init__.py
+++ facebook/__init__.py
@@ -45,7 +45,7 @@ __version__ = version.__version__
 FACEBOOK_GRAPH_URL = "https://graph.facebook.com/"
 FACEBOOK_WWW_URL = "https://www.facebook.com/"
 FACEBOOK_OAUTH_DIALOG_PATH = "dialog/oauth?"
-VALID_API_VERSIONS = ["3.1", "3.2", "3.3", "4.0", "5.0", "6.0", "7.0", "8.0", "9.0", "10.0"]
+VALID_API_VERSIONS = ["3.1", "3.2", "3.3", "4.0", "5.0", "6.0", "7.0", "8.0", "9.0", "10.0", "11.0"]
 VALID_SEARCH_TYPES = ["place", "placetopic"]
 
 
